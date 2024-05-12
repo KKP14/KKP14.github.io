2aa72514deb193a5d4224753c6bddea263290af1
KKP14 <124814482+KKP14@users.noreply.github.com>
2024-05-12 18:55:28 +0100

    Delete UI Assignment 2 directory

--- UI Assignment 2/scripts/ReviewPopup.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#submit").addEventListener("click",function(){document.body.classList.add("active-popup");});
-document.querySelector(".popupMessage .close-btn").addEventListener("click",function(){document.body.classList.remove("active-popup");});
