eac78a23c369b3301baa061e1729d0a26ff6596a
KKP14 <124814482+KKP14@users.noreply.github.com>
2024-05-12 18:47:56 +0100

    Add files via upload

---
+++ UI Assignment 2/scripts/ReviewPopup.js
@@ -0,0 +1,2 @@
+document.querySelector("#submit").addEventListener("click",function(){document.body.classList.add("active-popup");});
+document.querySelector(".popupMessage .close-btn").addEventListener("click",function(){document.body.classList.remove("active-popup");});
